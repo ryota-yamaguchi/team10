93b28baec4dc10678f04956eb8d00d0c30dabf42
Ryota Yamaguchi <ryota@seas-company.jp>
2015-10-27 19:12:01 +0900

    init.rbに初期項目を記述 #20151027-02-ryota

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :team10 do
   name 'Team10 plugin'
-  author 'Author name'
+  author 'Ryota Yamaguchi'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/ryota-yamaguchi/team10'
+  author_url 'https://github.com/ryota-yamaguchi/'
 end
